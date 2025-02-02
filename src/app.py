1925909bd05f210cff752e25fc879c2c2fab9d3c
STEPHEN MULINGWA <mulingwastephen200@gmail.com>
2025-02-02 13:46:18 +0300

    Made changes to Requirement file

--- src/app.py
+++ src/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 import pickle
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+import os
+
 
 with open('../scaler.pkl', 'rb') as f:
     scaler = pickle.load(f)  # Load the pre-trained scaler
@@ -82,4 +84,5 @@ def predict():
         return render_template('result.html', prediction=f"Error: {str(e)}")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use Render's dynamic port
+    app.run(host='0.0.0.0', port=port)
